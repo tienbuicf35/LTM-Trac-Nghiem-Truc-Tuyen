f533b2978da67141663c0c59ea9a3a5f04ef9949
tienbuicf35 <131645023+tienbuicf35@users.noreply.github.com>
2025-11-24 00:19:13 +0700

    Update Question.java

--- Quiz/Question.java
+++ Quiz/Question.java
@@ -3,19 +3,23 @@ package Quiz;
 import java.io.Serializable;
 import java.util.List;
 
+// Lớp này phải implement Serializable vì nó được gửi qua ObjectInputStream/ObjectOutputStream
 public class Question implements Serializable {
-    private static final long serialVersionUID = 1L;
-
+    // Để đảm bảo tương thích khi deserialize
+    private static final long serialVersionUID = 1L; 
+    
     private String questionText;
     private List<String> options;
-    private int correctAnswer;
+    private int correctAnswer; // Index của đáp án đúng (0, 1, 2, hoặc 3)
 
+    // Constructor 
     public Question(String questionText, List<String> options, int correctAnswer) {
         this.questionText = questionText;
         this.options = options;
         this.correctAnswer = correctAnswer;
     }
 
+    // Getters được sử dụng trong Quizclient
     public String getQuestionText() {
         return questionText;
     }
@@ -23,7 +27,7 @@ public class Question implements Serializable {
     public List<String> getOptions() {
         return options;
     }
-
+    
     public int getCorrectAnswer() {
         return correctAnswer;
     }
